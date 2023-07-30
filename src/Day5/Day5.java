96122528884440289b064fb80c0cb9ef7b0a2de1
samiahmadi <samiahmadi@localhost>
2023-07-29 22:54:17 -0400

    New Class

---
+++ src/Day5/Day5.java
@@ -0,0 +1,20 @@
+package Day5;
+
+public class Day5 {
+	
+	public static void main(String[] args) {
+
+	String title = "Director";
+	String author = "Ahmadi";
+	int pageNum = 200;
+	String isbn = "ALB0098RB";
+	int publishyear= 2023;
+	
+	System.out.println(title);
+	System.out.println(author);
+	System.out.println(pageNum);
+	System.out.println(isbn);
+	System.out.println(publishyear);	
+	
+	}
+}
